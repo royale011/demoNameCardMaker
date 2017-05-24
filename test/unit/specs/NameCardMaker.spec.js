45c822295114f786accfb96663a98106e06dea35
royale011 <27363134@qq.com>
2017-05-24 22:00:13 +0800

    [nero] add test for initial stage

--- test/unit/specs/NameCardMaker.spec.js
+++ test/unit/specs/NameCardMaker.spec.js
@@ -2,32 +2,43 @@ import Vue from 'vue'
 import Utils from './Utils'
 import NameCardMaker from '@/components/NameCardMaker'
 
-describe('NameCardMaker.vue', (done) => {
-  it('should show name in card when input name', () => {
+describe('NameCardMaker.vue', () => {
+  it('should have empty info when mounted', () => {
     const Constructor = Vue.extend(NameCardMaker)
     const vm = new Constructor().$mount()
-    vm.$el.querySelector('#name-input').value = 'name'
+    expect(vm.picValue).to.equal('')
+    expect(vm.cropper).to.equal('')
+    expect(vm.panel).to.equal(false)
+    expect(vm.imageUrl).to.equal('')
+    expect(vm.name).to.equal('')
+    expect(vm.email).to.equal('')
+    expect(vm.wechat).to.equal('')
+  })
+  it('should show name in card when input name', (done) => {
+    const Constructor = Vue.extend(NameCardMaker)
+    const vm = new Constructor().$mount()
+    vm.name = 'name'
     Utils.nextTick(done, 1, function () {
       expect(vm.$el.querySelector('#name').textContent)
         .to.equal('name')
     })
   })
-  it('should show email in card when input email', () => {
+  it('should show email in card when input email', (done) => {
     const Constructor = Vue.extend(NameCardMaker)
     const vm = new Constructor().$mount()
-    vm.$el.querySelector('#email-input').value = 'test@example.com'
+    vm.email = 'test@example.com'
     Utils.nextTick(done, 1, function () {
       expect(vm.$el.querySelector('#email').textContent)
         .to.equal('test@example.com')
     })
   })
-  it('should show wechat in card when input wechat', () => {
+  it('should show wechat in card when input wechat', (done) => {
     const Constructor = Vue.extend(NameCardMaker)
     const vm = new Constructor().$mount()
-    vm.$el.querySelector('#wechat-input').value = 'test@example.com'
+    vm.wechat = 'wechat'
     Utils.nextTick(done, 1, function () {
       expect(vm.$el.querySelector('#wechat').textContent)
-        .to.equal('test@example.com')
+        .to.equal('wechat')
     })
   })
 })
